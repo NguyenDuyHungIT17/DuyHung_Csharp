c847db96b296b520a14210e124c0eee958489c87
Nguyen Hung <nguyenduyhungIT17@gmail.com>
2025-08-11 10:05:49 +0700

    CSharp

---
+++ Nâng cao/Nang_Cao/LinQ/FINT_STUDENT.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE FINT_STUDENT;
+GO
+
+USE FINT_STUDENT;
+GO
+
+CREATE TABLE students (
+	MSSV NVARCHAR(10) PRIMARY KEY,
+    HoTen NVARCHAR(50),
+    DiemTB FLOAT
+)
+
+INSERT INTO students VALUES 
+('SV001', N'Nguyễn Văn A', 7.5),
+('SV002', N'Trần Thị B', 8.0),
+('SV003', N'Lê Văn C', 6.8);
+
+select *from students;
